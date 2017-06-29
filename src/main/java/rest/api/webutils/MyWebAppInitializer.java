f33742329b285702b93a3e2efca9ccf8ca1e45f1
bbkchdhry <bbkchdhry@gmail.com>
2017-06-30 00:27:47 +0545

    Added WebApplicationInitializer

---
+++ src/main/java/rest/api/webutils/MyWebAppInitializer.java
@@ -0,0 +1,16 @@
+package rest.api.webutils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class MyWebAppInitializer implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext arg0) throws ServletException {
+		
+		
+	}
+
+}
